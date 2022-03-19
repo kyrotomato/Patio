bc827a610a565b9be12cc2e5988e6ddcb42ea62a
MrCurls <JonCurls@gmail.com>
2022-03-19 12:31:24 -0500

    weatherCard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,12 @@
 //query selectors
-const burgerIcon = document.querySelector('#menu');
-const navbarMenu = document.querySelector('#nav-links');
-var zipSearchText = document.querySelector('#search-content');
-var searchButton = document.querySelector('#search-button');
-var map = document.getElementById('map');
-
+const burgerIcon = document.querySelector("#menu");
+const navbarMenu = document.querySelector("#nav-links");
+var zipSearchText = document.querySelector("#search-content");
+var searchButton = document.querySelector("#search-button");
+var map = document.getElementById("map");
 
 //get geolocation with zipcode
-var getGeoLoc = function(event) {
+var getGeoLoc = function (event) {
   //prevent auto refresh on click
   event.preventDefault();
 
@@ -17,78 +16,80 @@ var getGeoLoc = function(event) {
   //zipSearchText.value = "";
 
   //api url (geolocation)
-  var geoApiUrl = "http://api.openweathermap.org/geo/1.0/zip?zip=" + zipCode + "&appid=6c1a0cb9791079e8155bbd6c4f59e4da";
+  var geoApiUrl =
+    "http://api.openweathermap.org/geo/1.0/zip?zip=" +
+    zipCode +
+    "&appid=6c1a0cb9791079e8155bbd6c4f59e4da";
 
   //request for url
   fetch(geoApiUrl)
-  .then(function(response) {
-    if (response.ok) {
-      //convert response to json
-      response.json().then(function(data) {
-        
+    .then(function (response) {
+      if (response.ok) {
+        //convert response to json
+        response.json().then(function (data) {
+          console.log(data);
 
-        console.log(data);
+          //retrieve lon + lat info for zipcode
+          var longitude = data.lon;
+          var latitude = data.lat;
 
-        //retrieve lon + lat info for zipcode
-        var longitude = data.lon;
-        var latitude = data.lat;
+          getLocalWeather(longitude, latitude);
+          breweryQuery(longitude, latitude);
+        });
+      }
+    })
 
-        getLocalWeather(longitude, latitude);
-        breweryQuery(longitude, latitude);
-
-      });
-    }
-  })
-
-  //catch rejected api requests
-  .catch(function(error) {
-    //maybe display error on DOM instead of alert later on
-    console.log("Could not connect to GeoLocationAPI");
-  });
-
-}
+    //catch rejected api requests
+    .catch(function (error) {
+      //maybe display error on DOM instead of alert later on
+      console.log("Could not connect to GeoLocationAPI");
+    });
+};
 
 //pass geolocation info into weather api
-var getLocalWeather = function(longitude, latitude) {
-  
+var getLocalWeather = function (longitude, latitude) {
   //confirm data passed through
   console.log("lon:" + longitude, "lat:" + latitude);
 
-  var weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=6c1a0cb9791079e8155bbd6c4f59e4da";
+  var weatherApiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    latitude +
+    "&lon=" +
+    longitude +
+    "&units=imperial&appid=6c1a0cb9791079e8155bbd6c4f59e4da";
 
-  fetch(weatherApiUrl)
-  .then(function(response) {
+  fetch(weatherApiUrl).then(function (response) {
     if (response.ok) {
       //convert response to json
-      response.json().then(function(data) {
-        
-        //weather json 
+      response.json().then(function (data) {
+        //weather json
         console.log(data);
-
       });
     }
-  })
-  
-}
+  });
+};
 
 //https://www.openbrewerydb.org/
-var breweryQuery=function(longitude, latitude){
-    //per_page=# will set how many breweries per call
-    var breweryAPIUrl = "https://api.openbrewerydb.org/breweries?per_page=50&by_dist=" + latitude + "," + longitude;
-    fetch(breweryAPIUrl)
-    .then(function(response) {
-      if (response.ok) {
-        //convert response to json
-        response.json().then(function(data){
-          console.log(data);
-        })
-      }
-    });
-  }
+var breweryQuery = function (longitude, latitude) {
+  //per_page=# will set how many breweries per call
+  var breweryAPIUrl =
+    "https://api.openbrewerydb.org/breweries?per_page=50&by_dist=" +
+    latitude +
+    "," +
+    longitude;
+  fetch(breweryAPIUrl).then(function (response) {
+    if (response.ok) {
+      //convert response to json
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
+};
 
 //takes zipcode input and gets geolocation
-searchButton.addEventListener('click', getGeoLoc);
+searchButton.addEventListener("click", getGeoLoc);
 
-burgerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
+burgerIcon.addEventListener("click", () => {
+  navbarMenu.classList.toggle("is-active");
 });
