06f4e7e59e02f7b0a559ca822290b6d97dcadca0
MrCurls <JonCurls@gmail.com>
2022-03-22 16:09:23 -0500

    mixed content error

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,17 +20,17 @@ var zipHistory = document.querySelector("#lsHistory");
 var getGeoLoc = function (event) {
   //prevent auto refresh on click
   event.preventDefault();
-  
+
   //clear previous search content
   weatherCard.textContent = "";
   brewCardContEl.textContent = "";
 
   var zipCode = zipSearchText.value;
   console.log(zipCode);
- 
+
   //api url (geolocation)
   var geoApiUrl =
-    "http://api.openweathermap.org/geo/1.0/zip?zip=" +
+    "https://api.openweathermap.org/geo/1.0/zip?zip=" +
     zipCode +
     "&appid=6c1a0cb9791079e8155bbd6c4f59e4da";
 
@@ -182,11 +182,11 @@ var setWeatherInfo = function (data) {
 };
 
 //set search input to local storage
-var assignLocalStorage = function(event) {
+var assignLocalStorage = function (event) {
   //add search to variable
   var zipcodeHistory = zipSearchText.value;
   console.log(zipcodeHistory);
-  
+
   //reset input text field
   zipSearchText.value = "";
 
@@ -197,14 +197,14 @@ var assignLocalStorage = function(event) {
   localStorage.setItem("zipcode", JSON.stringify(searchHistory));
 
   printLocal();
-}
+};
 
-var printLocal = function() {
+var printLocal = function () {
   //check if there is anything in localStorage
   var localZip = localStorage.getItem("zipcode");
-  
-  //if localstorgae exists, parse it and assign the array values to the 
-  if(localZip) {
+
+  //if localstorgae exists, parse it and assign the array values to the
+  if (localZip) {
     //clear array to accept local storage info
     searchHistory = [];
 
@@ -213,15 +213,12 @@ var printLocal = function() {
 
     //assign array to searchHistory array
     searchHistory = localZip;
-    
-  }
-  else {
+  } else {
     return;
     //local storage print to p element modal
-    
   }
-  zipHistory.innerText=searchHistory;
-}
+  zipHistory.innerText = searchHistory;
+};
 
 //takes zipcode input and gets geolocation
 searchButton.addEventListener("click", getGeoLoc);
